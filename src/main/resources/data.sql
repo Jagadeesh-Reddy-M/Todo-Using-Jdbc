3c7f1aab48caacf4965f98f14186e86d106dd0d9
Jagadeesh Reddy M <mjagadeesh7120@gmail.com>
2023-03-18 12:37:11 +0530

    todos

---
+++ src/main/resources/data.sql
@@ -0,0 +1,12 @@
+INSERT INTO todolist (todo, priority, status)
+VALUES
+('Complete project report', 'High', 'Incomplete'),
+('Pay rent', 'High', 'Complete'),
+('Buy groceries', 'Medium', 'Incomplete'),
+('Call mom', 'Low', 'Incomplete'),
+('Schedule doctor appointment', 'Medium', 'Incomplete'),
+('Clean apartment', 'Low', 'Incomplete'),
+('Submit expense report', 'High', 'Incomplete'),
+('Renew library books', 'Low', 'Incomplete'),
+('Attend team meeting', 'Medium', 'Complete'),
+('Follow up with client', 'High', 'Incomplete');
